d78ec23980fb98ad9cd2cccf4dcab98e11f7d9a8
Oorjaa28 <katariaoorjaa@gmail.com>
2025-07-01 20:39:22 +0530

    Initial commit with working Flask app

--- app.py
+++ app.py
@@ -101,4 +101,7 @@ def panchayat():
 if __name__ == '__main__':
     print("✅ Creating database and table if not exists...")
     init_db()
-    app.run(debug=True)
+
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
